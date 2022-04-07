7c1f1780f98ac8bf17e7f98a7a27322483444f04
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-04-06 21:19:38 -0300

    corrigindo erro no setimo desafio

--- setimoDesafio.js
+++ setimoDesafio.js
@@ -1,7 +1,7 @@
 for(i = 0; i <= 20; i++){
     if(i==0){
         console.log(i + " é nulo");
-    }else if(i%2 == 0){
-        console.log(i + " é par");
+    }else if(i%2 == 1){
+        console.log(i + " é impar");
     }
 }
